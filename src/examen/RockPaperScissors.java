d124057f818ca9359a06f0a923818585bb6929d5
AzraelVicente <dam14.2024@gmail.com>
2024-02-26 13:36:12 +0100

    Proyecto documentado

--- src/examen/RockPaperScissors.java
+++ src/examen/RockPaperScissors.java
@@ -2,6 +2,17 @@ package examen;
 
 import java.util.*;
 
+/**
+ * Clase para jugar a piedra, papel o tijera.
+ * 
+ * @author DAM113
+ * @version 1.2
+ * 
+ * @param keyboard Sirve para ingresar las opciones por teclado con el uso de números del 1 al 3, incluidos estos.
+ * @param scanner Sirve para inicializar el scanner y leer lo que el usuario ingresa por teclado.
+ * @param optionList Sirve para elegir las opciones disponibles respecto al número ingresado.
+ * 
+ */
 public class RockPaperScissors {
 	private static int keyboard = 0;
 	private static Scanner scanner = new Scanner(System.in);
@@ -10,6 +21,11 @@ public class RockPaperScissors {
 	public RockPaperScissors() {
 	}
 
+	/**
+	 * Método main con el que se muestra un pequeño menú explicando las opciones, aquí se elige de forma aleatoria la
+	 *  opción del ordenador, se pone en común con la que hayas puesto, se llama al método para ver quién ha ganado y cierra el scanner.
+	 * 
+	 */
 	public static void main(String[] args) {
 		do {
 			System.out.println("Welcome to Rock, Paper, Scissors!");
@@ -23,8 +39,15 @@ public class RockPaperScissors {
 		System.out.println(RockPaperScissors.checkGame(userChoice, computerChoice));
 		scanner.close();
 	}
-
-	public static String checkGame(String userChoice, String computerChoice) {
+	
+	/**
+	 * Método por el que según la opción elegida por el usuario y el ordenador, se indica un mensaje para mostrar si has ganado,
+	 *  has perdido o si hay un empate.
+	 *  
+	 *  @param gameResult Un string vacío que luego se devuelve con el resultado del juego respectivamente.
+	 * 
+	 */
+	public static String checkGame(String userChoice, String computerChoice) {	
 		String gameResult = "";
 		if (userChoice.equals(computerChoice)) {
 			gameResult = "It's a tie!";
